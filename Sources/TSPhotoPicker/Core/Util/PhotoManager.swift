d13f08a15d318c4b246721f3f8d3af4904f13542
litangsong <leetangsong@icloud.com>
2022-11-25 17:22:53 +0800

    no message

--- Sources/TSPhotoPicker/Core/Util/PhotoManager.swift
+++ Sources/TSPhotoPicker/Core/Util/PhotoManager.swift
@@ -9,5 +9,35 @@ import UIKit
 import Photos
 import PhotosUI
 public final class PhotoManager: NSObject {
-   
+    public static let shared = PhotoManager()
+    
+    
+    var thumbnailLoadMode: ThumbnailLoadMode = .complete
+}
+
+
+extension NSNotification.Name {
+    static let ThumbnailLoadModeDidChange: NSNotification.Name = .init("ThumbnailLoadModeDidChange")
+}
+extension PhotoManager {
+    enum ThumbnailLoadMode {
+        case simplify
+        case complete
+    }
+    func thumbnailLoadModeDidChange(
+        _ mode: ThumbnailLoadMode
+    ) {
+        if thumbnailLoadMode == mode {
+            return
+        }
+        thumbnailLoadMode = mode
+//        if !needReload && !forceReload {
+//            return
+//        }
+//        NotificationCenter.default.post(
+//            name: .ThumbnailLoadModeDidChange,
+//            object: nil,
+//            userInfo: ["needReload": forceReload ? true : needReload]
+//        )
+    }
 }
